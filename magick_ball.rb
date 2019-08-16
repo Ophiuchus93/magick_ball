f5b5e0bde04308f20eb9090296ee6bbfa4eb2871
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-08-15 18:56:52 -0600

    Final Draft

--- magick_ball.rb
+++ magick_ball.rb
@@ -3,7 +3,7 @@ require "pry"
 
 class Magick_ball
   def initialize
-    @answer = [
+    @answers = [
       "Yes",
       "No",
       "Maybe",
@@ -17,7 +17,10 @@ class Magick_ball
       "Without a doubt",
       "Reply hazy, try again"
     ]
-      @random_answers = @answer.sample
+
+      @user_answers = @answers.clone, []
+
+      @random_answers = @answers.sample
 
     open  
     menu 
@@ -25,13 +28,13 @@ class Magick_ball
 
   def open
     puts "--- MAGICK 8 BALL ---".colorize(:magenta)
-    puts "What is your name?".colorize(:magenta)
-    print "> ".colorize(:magenta)
+    puts "What is your name?".colorize(:yellow)
+    print "> ".colorize(:yellow)
     name = gets.strip
-    puts "When is your birthday?".colorize(:magenta)
-    print "> ".colorize(:magenta)
+    puts "When is your birthday?".colorize(:yellow)
+    print "> ".colorize(:yellow)
     birthday = gets.strip
-    puts "Welcome, #{name} born on #{birthday}".colorize(:magenta)
+    puts "Welcome, #{name} born on #{birthday}".colorize(:yellow)
     sleep(1)
   end
   
@@ -39,8 +42,8 @@ class Magick_ball
     puts "Please select an option".colorize(:magenta)
     puts "1) Ask a question".colorize(:magenta)
     puts "2) Add an answer".colorize(:magenta)
-    puts "3) Reset answers".colorize(:magenta)
-    puts "4) View all answers".colorize(:magenta)
+    puts "3) View all answers".colorize(:magenta)
+    puts "4) Reset answers".colorize(:magenta)
     puts "5) EXIT".colorize(:magenta)
     print ">"
   choice = gets.to_i
@@ -49,25 +52,39 @@ class Magick_ball
       puts "What is your question?".colorize(:green)
       print"> "
       gets
+      puts "Shaking Magic 8 Ball".colorize(:cyan)
+      sleep(1)
       puts @random_answers
-      sleep(3)
+      sleep(2)
       menu
     when 2
-      puts "What answer would you like to add?"
+      puts "What answer would you like to add?".colorize(:blue)
       print "> "
       reply = gets.strip
-      @answer << reply
+      @user_answers << reply
       sleep(1)
       menu
     when 3
-      puts "Are you sure you want to reset the answers?"
-      print "> "
+      puts @answers
+      puts @user_answers
+      menu
     when 4
-      puts @answer
+      puts "Are you sure you want to reset the answers y/n?".colorize(:red)
+      print "> "
+      yes = gets.strip
+      if yes == "y"
+        @user_answers.clear
+        # binding.pry
+        menu
+      else
+        menu  
+      end  
     when 5
+      puts "Make your own destiny".colorize(:cyan)
+      sleep (2)
       exit(true)
     else
-      puts "Please try again"
+      puts "Please try again".colorize(:red)
       sleep(2)
       print `clear`
       menu
